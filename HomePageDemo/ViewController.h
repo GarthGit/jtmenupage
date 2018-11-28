d25509ec4c1719cc974d9f609208f26754a9f0dd
hoyifo <hoyifo@hoyifodemac-mini.local>
2018-11-28 09:26:15 +0800

    Initial Commit

---
+++ HomePageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomePageDemo
+//
+//  Created by hoyifo on 2018/11/28.
+//  Copyright © 2018 hoyifo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HomePageDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HomePageDemo
+//
+//  Created by hoyifo on 2018/11/28.
+//  Copyright © 2018 hoyifo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
